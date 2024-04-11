3e829c747846750515d1cc33886ab9d7542025f7
Thomas Naake <thomasnaake@gmx.de>
2024-04-11 11:05:49 +0200

    Update conversion.R

--- R/conversion.R
+++ R/conversion.R
@@ -98,16 +98,17 @@ biocrates <- function(file, sheet, ...) {
     cD <- xls[inds_name, seq_len(min(which(inds_met)) - 1)]
     cD_tmp <- cD
     colnames(cD_tmp) <- tolower(colnames(cD_tmp))
-    cD <- data.frame(name = make.names(cD_tmp[, "sample.identification"]), 
+    cD <- data.frame(
+        name = make.unique(make.names(cD_tmp[, "sample.identification"])), 
         name_original = cD_tmp[, "sample.identification"], cD)
-    rownames(cD) <- make.unique(cD[["name"]])
+    rownames(cD) <- cD[["name"]]
     
     ## create assay, set values of 0 to NA
     a <- xls[inds_name, inds_met]
     a <- as.matrix(a)
     mode(a) <- "numeric"
     a[a == 0] <- NA
-    rownames(a) <- make.unique(cD[["name"]])
+    rownames(a) <- cD[["name"]]
 
     ## create SummarizedExperiment and return
     SummarizedExperiment::SummarizedExperiment(assays = t(a), 
